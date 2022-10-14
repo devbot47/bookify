14429b0ae9d090bff7a6622e0a167d747f74efff
devbot <dev.bot47@gmail.com>
2022-10-15 03:10:52 +0500

    added home page view and minor refactoring in bookings controller and views using partials

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,17 @@
 class BookingsController < ApplicationController
 
+  before_action :set_attributes, :check_valid_workshop, :charge_for_booking, only: [:create]
+
   def create
-
-    @stripe_service    = StripeService.new
-    @workshop          = Workshop.find(params[:workshop_id])
-    @user              = current_user
-    @stripe_customer   = @stripe_service.find_or_create_user(@user)
-    @card              = @stripe_service.create_stripe_customer_card(card_params, @stripe_customer)
-    @amount_to_be_paid = params[:total_tickets].to_i * @workshop.registration_fee
-    @charge            = @stripe_service.create_stripe_charge(@amount_to_be_paid, @stripe_customer.id, @card.id, @workshop)
-    @booking           = Booking.new(booking_params.merge({amount_paid: @amount_to_be_paid, stripe_transaction_id: @charge.id}))
-
-    if @charge && @booking.save!
+    @booking = Booking.new(booking_params.merge(
+      {
+        amount_paid: @amount_to_be_paid,
+        stripe_transaction_id: @charge.id
+      }))
+    if @booking.save!
       BookingsMailer.booking_confirmation(@booking).deliver_now
       redirect_to workshop_path(@workshop), notice: "You have successfully booked #{@booking.total_tickets} tickets for #{@workshop.name} workshop."
     end
-
-    rescue Stripe::StripeError => error
-      redirect_to workshop_path(@workshop), alert: "#{error.message}"
-
   end
 
   private
@@ -31,4 +24,25 @@ class BookingsController < ApplicationController
     params.permit(:total_tickets, :user_id, :workshop_id)
   end
 
+  def set_attributes
+    @stripe_service    = StripeService.new
+    @workshop          = Workshop.find(params[:workshop_id])
+    @user              = current_user
+  end
+
+  def charge_for_booking
+    @stripe_customer   = @stripe_service.find_or_create_user(@user)
+    @card              = @stripe_service.create_stripe_customer_card(card_params, @stripe_customer)
+    @amount_to_be_paid = params[:total_tickets].to_i * @workshop.registration_fee
+    @charge            = @stripe_service.create_stripe_charge(@amount_to_be_paid, @stripe_customer.id, @card.id, @workshop)
+
+    rescue Stripe::StripeError => error
+      redirect_to workshop_path(@workshop), alert: "#{error.message}"
+  end
+
+  def check_valid_workshop
+    if @workshop.is_upcoming_workshop? || @workshop.remaining_seats - params[:total_tickets].to_i < 0
+      redirect_to workshop_path(@workshop), alert: "Sorry! cannot book your tickets. Please check remaining seats or start date for this workshop."
+    end
+  end
 end
--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -8,4 +8,11 @@ class Workshop < ApplicationRecord
             numericality: true
   validates :end_date, comparison: {greater_than: :start_date, message: 'cannot be before start date'}
 
+  scope :upcoming_workshops, -> { where('start_date > ?', Date.today) }
+  scope :past_workshops, -> { where('start_date < ?', Date.today) }
+
+  def is_upcoming_workshop?
+    start_date < Date.today
+  end
+
 end
