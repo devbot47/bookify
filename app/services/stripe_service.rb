0fe3c1821f90a8651acfb41e6d95f735158312a1
devbot <dev.bot47@gmail.com>
2022-10-14 15:37:55 +0500

    added stripe charge API

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -39,4 +39,14 @@ class StripeService
     )
   end
 
+  def create_stripe_charge(amount_to_paid, stripe_customer_id, card_id, workshop)
+    Stripe::Charge.create({
+            amount: amount_to_paid * 100,
+            currency: 'usd',
+            source: card_id,
+            customer: stripe_customer_id,
+            description: "Amount #{amount_to_paid} charged for #{workshop.name} workshop.",
+          })
+  end
+
 end
